ccbdd76826819a73e8025d5edd79ce0346b468fe
andresfabre27 <andres_fabre@hotmail.com>
2025-08-23 18:31:31 -0300

    Terminado ejercicio Nro 1
    Terminado el ejercicio Nro 1 del práctico 3

---
+++ Trabajo Practico Nro 3/TrabajoPracticoNro3.py
@@ -0,0 +1,11 @@
+#Trabajo Practico Nro 3
+#Tema: Condicionales
+#Nomnbre: Andrés Fabre, Cristian 
+
+#Ejercicio Nro 1
+edad=int(input("Ingresa tu edad: "))
+
+if edad>=18 :
+    print("Sos mayor de edad")
+
+

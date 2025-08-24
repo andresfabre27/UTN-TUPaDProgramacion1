9d168f904d74f1240142f3d9a168f6d2cfdd0cb7
andresfabre27 <andres_fabre@hotmail.com>
2025-08-24 18:41:07 -0300

    Terminado ejercicio Nr 10 y practico finalizado
    Terminado el ejercicio 10 del practico nro 3 y practico finalizado.

--- Temp/Temp.py
+++ Temp/Temp.py
@@ -1,18 +1,101 @@
-#Ejercicio Nro 9
+#Ejercicio Nro 10
 
-magnitud=float(input("Ingrese la magnitud del terremonto: "))
-print("Clasificacion: ")
+dia=int(input("Ingresa el dia: "))
+mes=input("Ingresa el mes: ").lower()
+emisferio=input("Ingresa el Emisferio, N o S: ").upper()
 
-if magnitud<3 :
-    print("Muy leve")
-elif magnitud>=3 and magnitud<4 :
-    print("Ligeramente perceptible")
-elif magnitud>=4 and magnitud<5 :
-    print("Moderado")
-elif magnitud>=5 and magnitud<6 :
-    print("Fuerte")
-elif magnitud>=6 and magnitud<7 :
-    print("Muy Fuerte")
+if emisferio=="N" :
+   if mes=="diciembre" :
+      if dia>=21 and dia<=31 :
+         print("Invierno")
+   elif mes=="enero" :
+      if dia>=1 and dia <=31 :
+         print("Invierno")
+   elif mes=="febrero" :
+      if dia>=1 and dia <=28 :
+         print("Invierno")
+   elif mes=="marzo" :
+      if dia>=1 and dia <=20 :
+         print("Invierno")
+      elif dia>=21 and dia<=31 :
+         print("Primavera")
+   elif mes=="abril" :
+      if dia>=1 and dia<=30 :
+         print("Primavera")
+   elif mes=="mayo" :
+      if dia>=1 and dia<=31 :
+         print("Primavera")
+   elif mes=="junio" :
+      if dia>=1 and dia<=20 :
+         print("Primavera")
+      elif dia>=21 and dia<=30 :
+         print("Verano")
+   elif mes=="julio" :
+      if dia>=1 and dia<=31 :
+         print("Verano")
+   elif mes=="agosto" :
+      if dia>=1 and dia<=31 :
+         print("Verano")
+   elif mes=="septiembre" :
+      if dia>=1 and dia<=20 :
+         print("Verano")
+      elif dia>=21 and dia<=30 :
+         print("Otoño")
+   elif mes=="octubre" :
+      if dia>=1 and dia<=31 :
+         print("Otoño")
+   elif mes=="noviembre" :
+      if dia>=1 and dia<=30 :
+         print("Otoño")
+   elif mes=="diciembre" :
+      if dia>=1 and dia<=20 :
+         print("Otoño")
+elif emisferio=="S" :
+   if mes=="diciembre" :
+      if dia>=21 and dia<=31 :
+         print("Verano")
+   elif mes=="enero" :
+      if dia>=1 and dia <=31 :
+         print("Verano")
+   elif mes=="febrero" :
+      if dia>=1 and dia <=28 :
+         print("Verano")
+   elif mes=="marzo" :
+      if dia>=1 and dia <=20 :
+         print("Verano")
+      elif dia>=21 and dia<=31 :
+         print("Otoño")
+   elif mes=="abril" :
+      if dia>=1 and dia<=30 :
+         print("Otoño")
+   elif mes=="mayo" :
+      if dia>=1 and dia<=31 :
+         print("Otoño")
+   elif mes=="junio" :
+      if dia>=1 and dia<=20 :
+         print("Otoño")
+      elif dia>=21 and dia<=30 :
+         print("Invierno")
+   elif mes=="julio" :
+      if dia>=1 and dia<=31 :
+         print("Invierno")
+   elif mes=="agosto" :
+      if dia>=1 and dia<=31 :
+         print("Invierno")
+   elif mes=="septiembre" :
+      if dia>=1 and dia<=20 :
+         print("Invierno")
+      elif dia>=21 and dia<=30 :
+         print("Primavera")
+   elif mes=="octubre" :
+      if dia>=1 and dia<=31 :
+         print("Primavera")
+   elif mes=="noviembre" :
+      if dia>=1 and dia<=30 :
+         print("Primavera")
+   elif mes=="diciembre" :
+      if dia>=1 and dia<=20 :
+         print("Primavera")
 else :
-    print("Exremo")
-
+   print("Fecha incorrecta")
+      
--- Trabajo Practico Nro 3/TrabajoPracticoNro3.py
+++ Trabajo Practico Nro 3/TrabajoPracticoNro3.py
@@ -110,3 +110,106 @@ elif magnitud>=6 and magnitud<7 :
     print("Muy Fuerte")
 else :
     print("Exremo")
+
+
+#Ejercicio Nro 10
+
+dia=int(input("Ingresa el dia: "))
+mes=input("Ingresa el mes: ").lower()
+emisferio=input("Ingresa el Emisferio, N o S: ").upper()
+
+if emisferio=="N" :
+   if mes=="diciembre" :
+      if dia>=21 and dia<=31 :
+         print("Invierno")
+   elif mes=="enero" :
+      if dia>=1 and dia <=31 :
+         print("Invierno")
+   elif mes=="febrero" :
+      if dia>=1 and dia <=28 :
+         print("Invierno")
+   elif mes=="marzo" :
+      if dia>=1 and dia <=20 :
+         print("Invierno")
+      elif dia>=21 and dia<=31 :
+         print("Primavera")
+   elif mes=="abril" :
+      if dia>=1 and dia<=30 :
+         print("Primavera")
+   elif mes=="mayo" :
+      if dia>=1 and dia<=31 :
+         print("Primavera")
+   elif mes=="junio" :
+      if dia>=1 and dia<=20 :
+         print("Primavera")
+      elif dia>=21 and dia<=30 :
+         print("Verano")
+   elif mes=="julio" :
+      if dia>=1 and dia<=31 :
+         print("Verano")
+   elif mes=="agosto" :
+      if dia>=1 and dia<=31 :
+         print("Verano")
+   elif mes=="septiembre" :
+      if dia>=1 and dia<=20 :
+         print("Verano")
+      elif dia>=21 and dia<=30 :
+         print("Otoño")
+   elif mes=="octubre" :
+      if dia>=1 and dia<=31 :
+         print("Otoño")
+   elif mes=="noviembre" :
+      if dia>=1 and dia<=30 :
+         print("Otoño")
+   elif mes=="diciembre" :
+      if dia>=1 and dia<=20 :
+         print("Otoño")
+elif emisferio=="S" :
+   if mes=="diciembre" :
+      if dia>=21 and dia<=31 :
+         print("Verano")
+   elif mes=="enero" :
+      if dia>=1 and dia <=31 :
+         print("Verano")
+   elif mes=="febrero" :
+      if dia>=1 and dia <=28 :
+         print("Verano")
+   elif mes=="marzo" :
+      if dia>=1 and dia <=20 :
+         print("Verano")
+      elif dia>=21 and dia<=31 :
+         print("Otoño")
+   elif mes=="abril" :
+      if dia>=1 and dia<=30 :
+         print("Otoño")
+   elif mes=="mayo" :
+      if dia>=1 and dia<=31 :
+         print("Otoño")
+   elif mes=="junio" :
+      if dia>=1 and dia<=20 :
+         print("Otoño")
+      elif dia>=21 and dia<=30 :
+         print("Invierno")
+   elif mes=="julio" :
+      if dia>=1 and dia<=31 :
+         print("Invierno")
+   elif mes=="agosto" :
+      if dia>=1 and dia<=31 :
+         print("Invierno")
+   elif mes=="septiembre" :
+      if dia>=1 and dia<=20 :
+         print("Invierno")
+      elif dia>=21 and dia<=30 :
+         print("Primavera")
+   elif mes=="octubre" :
+      if dia>=1 and dia<=31 :
+         print("Primavera")
+   elif mes=="noviembre" :
+      if dia>=1 and dia<=30 :
+         print("Primavera")
+   elif mes=="diciembre" :
+      if dia>=1 and dia<=20 :
+         print("Primavera")
+else :
+   print("Fecha incorrecta")
+      

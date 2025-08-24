5f604cb4fcbcfea635d0926cd740bceeb14bb8b3
andresfabre27 <andres_fabre@hotmail.com>
2025-08-24 15:17:14 -0300

    Terminado ejercicio nr 8
    Terminado el ejercicio 8 del practico nro 3

--- Trabajo Practico Nro 3/TrabajoPracticoNro3.py
+++ Trabajo Practico Nro 3/TrabajoPracticoNro3.py
@@ -92,3 +92,20 @@ if ultima_letra in vocal :
     print(palabra_final)
 else :
     print(palabra) 
+
+#Ejercicio Nro 8
+
+nombre=input("Ingrese su nombre: ")
+print(""" 1- Si quiere su nombre en mayúsculas.
+ 2- Si quiere su nombre en minúsculas.
+ 3- Si quiere su nombre con la primera letra mayúscula.""")
+menu=int(input("Ingrese el numero: "))
+
+if menu==1 :
+    print(nombre.upper())
+elif menu==2 :
+    print(nombre.lower())
+elif menu==3 :
+    print(nombre.title())
+else :
+    print("No elijio una opción valida")

94e1ca24f3a94e5b90f0af507bef658f50b164c2
CristZun <cristianzunigafabian@gmail.com>
2025-08-31 18:59:44 -0300

    se realizan actividades

--- TrabajoCooperativo Nro 1/Cristian.py
+++ TrabajoCooperativo Nro 1/Cristian.py
@@ -2,7 +2,7 @@
 #asigne el mismo a una variable valorDecimal, aplique las operaciones de CASTING para 
 #convertir la variable en otro tipo de dato. Investigue las diferentes formas de lograr la 
 #conversión. Muestre por pantalla el resultado de las conversiones.  
-
+"""
 valorDecimal = float(input("Ingrese un número decimal: "))
 #entero
 valorEntero = int(valorDecimal)
@@ -24,3 +24,58 @@ print("Convertido a lista (list):", valorLista)
 #y por medio del uso de las operaciones matemáticas módulo 10 y división por 10 
 #efectué la suma de los 3 dígitos del número. Ejemplo ingreso 563, salida del algoritmo 
 #14. Plantee el algoritmo planteando métodos para su resolución.
+
+numero=int(input("ingrese un numero de 3 digitos "))
+suma=0
+if numero >99 and numero < 1000:
+    while numero > 0:
+        digito=numero%10
+        suma=suma+digito
+        numero=int(numero//10)
+else:   
+    print("El número debe ser entre 100 y 999")
+    
+print(f"La suma del valor ingresado es {suma}")
+
+#5- Solicite el ingreso de una cadena y elimine todos los espacios de la misma, muestre 
+#la cadena resultante.
+
+frase=input("Ingrese una frase ")
+frase_sin_espacios =frase.replace(" ","") #metodo para remplazar
+print(f"Su frase sin espacios es: {frase_sin_espacios}")
+
+#7- Solicite el ingreso de una cadena y determine el tamaño de la misma y cuantas 
+#vocales tiene en total. 
+
+frase = input("Ingrese una frase ").lower()
+
+print(f"El tamaño de su cadena es de {len(frase)}")
+
+total_vocales=0
+
+for v in "aeiou": #recorre la frase tomando cada vocal en v.
+    total_vocales+=frase.count(v)
+
+print(f"El total de las vocales en la frasa es de: {total_vocales}")
+
+#9- Recorre la cadena del ejercicio 6 y transforma cada carácter a su código ASCII. 
+#Muéstralos en línea recta, separados por un espacio entre cada carácter.
+ 
+cadena = "La lluvia en Mendoza es escasa"
+
+for caracter in cadena:
+    print(ord(caracter), end=" ")  
+    # ord transforma un caracter en un ASCII, devuelve un numero entero
+    #end=" " evita el salto de línea y pone un espacio
+
+print()  # para dejar una línea vacía al final
+
+#11- Pedir dos palabras por teclado, indicar si son iguales. 
+"""
+palabra1 =input("Ingrese la primer palabra")
+palabra2 =input("Ingrese la segunda palabra")
+
+if palabra1 == palabra2:
+    print("las frases son identicas")
+else:
+    print("las Frases son diferentes") 

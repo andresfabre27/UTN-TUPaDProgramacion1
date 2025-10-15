7d75a6211a815d81b9d4d40e1ec962610a894642
andresfabre27 <andres_fabre@hotmail.com>
2025-10-14 23:20:11 -0300

    Ejercicio 6 tp Recurcion listo

--- Temp/Temp.py
+++ Temp/Temp.py
@@ -1,19 +1,5 @@
-#EJercicio Nro 4
+#EJercicio Nro 6
 
-def convertir_binario(numero):
-    lista=[]
+def suma_digitos(n):
 
-    while(numero>0):
-        
-        resto=numero%2
-        lista.append(resto)
-        numero=numero//2
     
-    invertido=lista[::-1]
-    cadena_de_numeros = "".join(str(x) for x in invertido)
-    return cadena_de_numeros
-
-
-numero=int(input("Ingrese un numero para convertirlo en binario: "))
-print(convertir_binario(numero))
-

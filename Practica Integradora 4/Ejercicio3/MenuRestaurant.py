2d245935301e9218f16a9904dc5c6735bf69f38c
andresfabre27 <andres_fabre@hotmail.com>
2025-10-18 21:56:17 -0300

    Comenzando ejercicio 3 de POO

---
+++ Practica Integradora 4/Ejercicio3/MenuRestaurant.py
@@ -0,0 +1,11 @@
+
+
+class MenuRestaurant:
+
+    def __init__(self):
+
+        self.platos=[]
+
+    def agregar_platos(self,dato):
+
+        self.platos.append(dato)
---
+++ Practica Integradora 4/Ejercicio3/Plato.py
@@ -0,0 +1,10 @@
+class plato:
+    def __init__(self, nombreCompleto, precio, esBebida):
+
+        self.nombreCompleto=nombreCompleto
+        self.precio=precio
+        self.esBebida=esBebida
+        self.lista_de_ingredientes=[]
+
+    def agregar_ingredientes(self,valor):
+        self.lista_de_ingredientes.append(valor)

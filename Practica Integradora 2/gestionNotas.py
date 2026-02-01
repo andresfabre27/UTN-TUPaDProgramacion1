28ebac78d5d6cf397e6d445f3039ed150bfa3a18
andresfabre27 <andres_fabre@hotmail.com>
2026-01-31 21:09:53 -0300

    Correcion TP integrador 2
    Correcion TP integrador 2

--- Practica Integradora 2/gestionNotas.py
+++ Practica Integradora 2/gestionNotas.py
@@ -95,3 +95,98 @@ def mejor_promedio(mejorpromedio):
 
 
 iterar_alumnos(alumnos,materias,notasFinales)
+
+
+
+
+
+
+#Practica integradora 2 Correciones y codigo completo (sin validaciones)
+
+alumnos={60902 : "Rodolfo Fernandez",
+         61654 : "Luis Gomez",
+         61852 : "Andrea Pereira",
+         61754 : "Juan Cruz Gonzales"
+        }
+
+materias=[["Ciencias",None,None,None],
+          ["Historia",None,None,None],
+          ["Geografia",None,None,None],
+          ["Matematicas",None,None,None],
+          ["Fisica",None,None,None]
+          ]
+
+notasFinales=[]
+
+def cargar_notas_Main(alumnos,materias,notasFinales):
+
+    print("Carga de Notas para los alumnos") 
+    for diccionario in alumnos.values():
+        print(f"Alumno: {diccionario}")
+        contador=0
+        for linea in materias:
+            print(f"Materia: {linea[0]}")
+            nota1=float(input("Ingrese la primer nota: "))
+            materias[contador][1]=nota1
+            nota2=float(input("Ingrese la segunda nota: "))
+            materias[contador][2]=nota2
+            promedio=(nota1+nota2)/2
+            materias[contador][3]=promedio
+            contador+=1
+        mostrar_materias(materias)
+        print(f"La materia con mejor nota es: {materia_mejor_calificacion(materias)}")
+        print(f"El promedio general del alumno es: {promedio_general(materias,diccionario,notasFinales)}")
+    print(f"El alumno con mejor promedio es: {mejor_promedio(notasFinales)}")
+
+def mostrar_materias(materias):
+
+    for linea in materias:
+        print(f"Materia: {linea[0]} Nota 1: {linea[1]} Nota 2: {linea[2]}")
+
+def materia_mejor_calificacion(materias):
+
+    mejor=0
+    mejorMateria=[]
+
+    for linea in materias:
+        if linea[3]>=mejor:
+            mejor=linea[3]
+
+    for linea in materias:
+            if linea[3]==mejor:
+                mejorMateria.append(linea[0])
+
+    return mejorMateria 
+
+def promedio_general(materias,diccionario,notasFinales):
+    
+    promedio=0
+    for linea in materias:
+        promedio+=linea[3]
+
+    promedio/=5
+    lista=[diccionario,promedio]
+    notasFinales.append(lista)
+
+    return promedio
+
+def mejor_promedio(notasFinales):
+    
+    notaMayor=0
+    mejorAlumno=[]
+    for linea in notasFinales:
+        if linea[1]>notaMayor:
+            notaMayor=linea[1]
+
+    for linea in notasFinales:
+        if linea[1]==notaMayor:
+            mejorAlumno.append(linea[0])
+    return mejorAlumno
+        
+        
+
+cargar_notas_Main(alumnos,materias,notasFinales)
+
+
+
+        
--- Temp/Temp.py
+++ Temp/Temp.py
@@ -1,38 +1,92 @@
+#Practica integradora 2
 
-<<<<<<< Updated upstream
+alumnos={60902 : "Rodolfo Fernandez",
+         61654 : "Luis Gomez",
+         61852 : "Andrea Pereira",
+         61754 : "Juan Cruz Gonzales"
+        }
 
-matriz=[[1,2,3],
-        [4,5,6],
-        [7,8,9]]
+materias=[["Ciencias",None,None,None],
+          ["Historia",None,None,None],
+          ["Geografia",None,None,None],
+          ["Matematicas",None,None,None],
+          ["Fisica",None,None,None]
+          ]
 
-numero=int(input("Ingrese del 1 al 9: "))
+notasFinales=[]
 
+def cargar_notas_Main(alumnos,materias,notasFinales):
 
-"""for i in range(len(matriz)):
-    if numero==matriz[i][0]:
-        posicion=i"""
+    print("Carga de Notas para los alumnos") 
+    for diccionario in alumnos.values():
+        print(f"Alumno: {diccionario}")
+        contador=0
+        for linea in materias:
+            print(f"Materia: {linea[0]}")
+            nota1=float(input("Ingrese la primer nota: "))
+            materias[contador][1]=nota1
+            nota2=float(input("Ingrese la segunda nota: "))
+            materias[contador][2]=nota2
+            promedio=(nota1+nota2)/2
+            materias[contador][3]=promedio
+            contador+=1
+        mostrar_materias(materias)
+        print(f"La materia con mejor nota es: {materia_mejor_calificacion(materias)}")
+        print(f"El promedio general del alumno es: {promedio_general(materias,diccionario,notasFinales)}")
+    print(f"El alumno con mejor promedio es: {mejor_promedio(notasFinales)}")
 
-contador=0
-for linea in matriz:
-    if numero in linea:
-        posicion=contador
-    contador+=1
+def mostrar_materias(materias):
 
-print(f"El numero esta en la posicion {posicion}") 
+    for linea in materias:
+        print(f"Materia: {linea[0]} Nota 1: {linea[1]} Nota 2: {linea[2]}")
 
+def materia_mejor_calificacion(materias):
 
+    mejor=0
+    mejorMateria=[]
 
-=======
-def fibonaci(n):
+    for linea in materias:
+        if linea[3]>=mejor:
+            mejor=linea[3]
 
-    if n==0 or n==1:
-        return n
+    for linea in materias:
+            if linea[3]==mejor:
+                mejorMateria.append(linea[0])
+
+    return mejorMateria 
+
+def promedio_general(materias,diccionario,notasFinales):
     
-    else:
-        return fibonaci(n-1)+fibonaci(n-2)
-    
-numero=int(input("Ingrese hasta que posicion se calculara la serie de Fibonacci: "))
+    promedio=0
+    for linea in materias:
+        promedio+=linea[3]
 
-for i in range(numero+1):
-    print(f"{fibonaci(i)}",end=" ")
->>>>>>> Stashed changes
+    promedio/=5
+    lista=[diccionario,promedio]
+    notasFinales.append(lista)
+
+    return promedio
+
+def mejor_promedio(notasFinales):
+    
+    notaMayor=0
+    mejorAlumno=[]
+    for linea in notasFinales:
+        if linea[1]>notaMayor:
+            notaMayor=linea[1]
+
+    for linea in notasFinales:
+        if linea[1]==notaMayor:
+            mejorAlumno.append(linea[0])
+    return mejorAlumno
+        
+        
+
+cargar_notas_Main(alumnos,materias,notasFinales)
+
+
+
+        
+
+
+
